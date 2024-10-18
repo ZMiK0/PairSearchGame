b5db6c2d4a289a56e029842231e6eee9f31ee725
ZMiK0 <kbelz21z@gmail.com>
2024-10-18 20:21:34 +0200

    WIP

--- board.py
+++ board.py
@@ -6,13 +6,26 @@ class Board:
     rows (int): Number of rows.
     columns (int): Number of columns.
     symbols (list): List of symbols.
+    board (list): Board.
     '''
 
     def __init__(self, _row, _cols):
+        '''
+        Initializes the engine.
+
+        Parameters:
+        _row (int): Number of rows.
+        _cols (int): Number of columns.
+        '''
         self.rows = _row
         self.columns = _cols
         self.symbols = [" "," "," "," "," "," "," "," "," "," "," "," "," "," "," "]
+        self.board = []
         
+    #Hacer método getValidPos() para hacer pos1 = getValidPos()
+    #La board quedaba [[,,,,],[,,,,],[,,,,],[,,,,]]
+
+
     
 
     
