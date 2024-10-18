2bbcb70380a228683450d5ef3e692e7fc1e5b6f7
ZMiK0 <kbelz21z@gmail.com>
2024-10-18 19:46:21 +0200

    FromStart

--- main.py
+++ main.py
@@ -1,21 +1,5 @@
 import board
-import os
 
-def clear():
-    os.system("clear")
+b = board.Board(3,3)
 
-end = False
-
-
-player1Name = input("Player1: ")
-player2Name = input("Player2: ")
-
-row = int(input("Rows: "))
-col = int(input("Columns: "))
-gm = board.board(row, col, player1Name, player2Name)
-clear()
-gm.printBoard()
-input("")
-while not end:
-    clear()    
-    gm.turns()
+print(b.getRows())
--- player.py
+++ player.py
@@ -1,15 +1,52 @@
-class Player():
-    name = ""
-    score = 0
-    def __init__(self, name):
-        self.name = name
-        self.score = 0
+class Player:
+    '''
+    This is the player class.
 
+    Attributes:
+    score (int): Score of the player.
+    name (str): Name of the player.
+    isBot (boolean): Bot status.
+    '''
+
+    def __init__(self, _name, _isBot):
+        '''
+        Initializes the player.
+
+        Parameters:
+        _name (str): Player name.
+        _isBot (boolean): Determines if the player is a bot.
+        '''
+        self.score = 0
+        self.name = _name
+        self.isBot = _isBot
+
+    def addScore(self):
+        '''
+        It adds one point to the score.
+        '''
+        self.score += 1
+    
     def getName(self):
+        '''
+        Gets the player's name.
+
+        Returns:
+        str: Player name
+        '''
         return self.name
     
-    def setScorePlusOne(self):
-        self.score += 1
-
     def getScore(self):
+        '''
+        Gets the player's score.
+
+        Returns:
+        int: Player score
+        '''
         return self.score
+    
+    def botTurn(self):
+        '''
+        Imitate the player action. NOT FINISHED
+        '''
+        pass
+    
